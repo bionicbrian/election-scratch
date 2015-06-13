dcc6ef9c6b7faaec6cfa337641c5852753b7425a
Brian Moore <bmoore@book.com>
2015-06-13 12:49:13 -0400

    Refactor the “store” as a rough Poll model

--- src/js/stores/PollsInMemory.js
+++ src/js/stores/PollsInMemory.js
@@ -7,52 +7,84 @@ import enums from "../enums";
 import Candidate from "./Candidate";
 import { makeEmitter } from "pubit-as-promised";
 
-function Store() {
+var store = {
+    candidates: [],
+    add(type, data) {
+        this[type].push(data);
+    },
+    remove(type, id) {
+        newList = this[type].map((item) => item.id !== id);
+        this[type] = newList;
+    }
+};
+
+function Poll(store) {
     var publish = makeEmitter(this, ["CHANGE"]);
+    var socket;
 
     this.publish = publish;
-    this.candidates = [];
+
+    var sEmit = (type, data) => {
+        socket.emit("candidate-add", data);
+    };
+
+    this.attachSocket = (newSocket) => {
+        socket = newSocket;
+    };
+
+    var findTheCandidate = (candidateId) => _.findWhere(store.candidates, { id: candidateId });
+
+    this.addCandidate = (spec) => {// { name, link }) {
+        var c = new Candidate(spec);
+        store.add("candidates", c);
+        sEmit("candidate-add", c);
+        return Q.resolve();
+    };
+
+    this.removeCandidate = ({ candidateId }) => {
+        store.remove("candidates", candidateId);
+        return Q.resolve();
+    };
+
+    this.castVote = ({ candidateId, value }) => {
+        var candidate = findTheCandidate(candidateId);
+        var userVoteExists = candidate.votes.some((vote) => vote.ballotId === ballotId);
+
+        if (!userVoteExists) {
+            candidate.hasLocalVote = true;
+            var vote = { value, ballotId };
+            candidate.votes.push(vote);
+        }
+
+        return Q.resolve();
+    };
+
+    this.retractVote = ({ candidateId }) => {
+        var candidate = findTheCandidate(candidateId);
+        var newVotes = candidate.votes.filter((vote) => {
+            var doesNotHaveLocalVote = vote.ballotId !== ballotId;
+            if (!doesNotHaveLocalVote) {
+                candidate.hasLocalVote = false;
+            }
+            return doesNotHaveLocalVote;
+        });
+
+        candidate.votes = newVotes;
+
+        return Q.resolve();
+    };
+
+    Object.defineProperty(this, "candidates", {
+        get() {
+            return store.candidates;
+        },
+        enumerable: true
+    });
 }
 
-var store = new Store();
+var poll = new Poll(store);
 
 var ballotId = 123; // _.unique("ballot_"); or SHA2 or something
-
-var findTheCandidate = (candidateId) => _.findWhere(store.candidates, { id: candidateId });
-
-var addCandidate = (spec) => {// { name, link }) {
-    var c = new Candidate(spec);
-    store.candidates.push(c);
-};
-
-var removeCandidate = ({ candidateId }) => {
-    store.candidates = store.candidates.filter((candidate) => candidate.id !== candidateId);
-};
-
-var castVote = ({ candidateId, value }) => {
-    var candidate = findTheCandidate(candidateId);
-    var userVoteExists = candidate.votes.some((vote) => vote.ballotId === ballotId);
-
-    if (userVoteExists) {
-        return;
-    }
-
-    candidate.hasLocalVote = true;
-    var vote = { value, ballotId };
-    candidate.votes.push(vote);
-};
-
-var retractVote = ({ candidateId }) => {
-    var candidate = findTheCandidate(candidateId);
-    var newVotes = candidate.votes.filter((vote) => {
-        var doesNotHaveLocalVote = vote.ballotId !== ballotId;
-        if (!doesNotHaveLocalVote) {
-            candidate.hasLocalVote = false;
-        }
-        return doesNotHaveLocalVote;
-    });
-    candidate.votes = newVotes;
-};
 
 AppDispatcher.register(function (payload) {
     var type = payload.type;
@@ -60,22 +92,22 @@ AppDispatcher.register(function (payload) {
 
     switch (type) {
         case enums.ADD_CANDIDATE:
-            promise = Q.when(addCandidate(payload.data));
+            promise = poll.addCandidate(payload.data);
             break;
         case enums.REMOVE_CANDIDATE:
-            promise = Q.when(removeCandidate(payload.data));
+            promise = poll.removeCandidate(payload.data);
             break;
         case enums.CAST_VOTE:
-            promise = Q.when(castVote(payload.data));
+            promise = poll.castVote(payload.data);
             break;
         case enums.RETRACT_VOTE:
-            promise = Q.when(retractVote(payload.data));
+            promise = poll.retractVote(payload.data);
             break;
         default:
             break;
     }
 
-    promise.done(() => store.publish("CHANGE"));
+    promise.done(() => poll.publish("CHANGE"));
 });
 
-export default store;
+export default poll;
