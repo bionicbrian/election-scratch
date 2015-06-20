00322575c4d97e1b068d460bd0622a5afd271b0e
Brian Moore <bmoore@book.com>
2015-06-20 13:14:22 -0400

    Remove the idea of gateway for now

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,6 @@ import React from "react";
 import Poll from "./components/Poll";
 import PollModel from "./models/Poll";
 import CandidateModel from "./models/Candidate";
-// import Gateway from "./gateways/PollGateway";
 import { makeEmitter } from "pubit-as-promised";
 
 class SocketStore {
@@ -49,7 +48,6 @@ var socket = global.io();
 var store = new SocketStore(socket);
 
 var ballot = new Ballot();
-// var gateway = new Gateway(socket);
 var candidateFactory = (...args) => new CandidateModel(...args);
 var poll = new PollModel({ ballot, candidateFactory, store });
 
