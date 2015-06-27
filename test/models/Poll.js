3dabfc5277c7c062ffbc5c6df2db4e757cf2ccff
Brian Moore <bmoore@book.com>
2015-06-27 16:58:05 -0400

    Test the Poll model!

--- test/models/Poll.js
+++ test/models/Poll.js
@@ -1,5 +1,52 @@
+import Poll from "../../src/js/models/Poll";
+
+var candidateStub;
+
+var candidateFactory = ({ name, link }) => {
+    candidateStub = {
+        name,
+        link: link || "",
+        id: 123,
+        votes: []
+    };
+
+    return candidateStub;
+};
+
 describe("Poll Model", function () {
-    it("runs this test", function () {
-        (1 + 1).should.equal(3);
+    beforeEach(function () {
+        this.poll = new Poll({ candidateFactory, ballot: { id: 123 } });
+    });
+
+    it("should respond to some methods", function () {
+        this.poll.should.respondTo("addCandidate");
+        this.poll.should.respondTo("removeCandidate");
+    });
+
+    describe("Candidate events", function () {
+        describe("Candidate add", function () {
+            beforeEach(function () {
+                this.addSpy = sinon.stub();
+                this.poll.on("candidate-add", this.addSpy.bind(this));
+                this.poll.addCandidate({ name: "Brian" });
+            });
+
+            it("The candidate-add listener is called with correct payload", function () {
+                this.addSpy.should.have.been.calledWith({ candidate: candidateStub, pollId: "poll_2" });
+            });
+        });
+
+        describe("Candidate remove", function () {
+            beforeEach(function () {
+                this.removeSpy = sinon.stub();
+                this.poll.on("candidate-remove", this.removeSpy.bind(this));
+                this.poll.addCandidate({ name: "Brian" });
+                this.poll.removeCandidate({ candidateId: 123 });
+            });
+
+            it("The candidate-remove listener is called with correct payload", function () {
+                this.removeSpy.should.have.been.calledWith({ candidate: candidateStub, pollId: "poll_3" });
+            });
+        });
     });
 });
