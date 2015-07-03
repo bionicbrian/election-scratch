4132bdc06cd609c9e38b1037d5e3a45bd5964959
Brian Moore <bmoore@book.com>
2015-07-03 14:02:41 -0700

    Update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,17 @@ var browserify = require("browserify");
 var babelify = require("babelify");
 var watch = require("gulp-watch");
 
-gulp.task("bundle-js", function() {
+function bundle() {
     return browserify("./src/js/main.js")
             .transform(babelify)
             .bundle()
             .pipe(source("bundle.js"))
             .pipe(gulp.dest("./public/js"));
-});
+}
+
+gulp.task("bundle-js", bundle);
 
 gulp.task("default", function () {
+    bundle();
     gulp.watch("./src/js/**/*.js", ["bundle-js"]);
 });
